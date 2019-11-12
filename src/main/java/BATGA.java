64be11fa369740b0a57a72f66980d4543d41eafb
Ulrike Klaus <55379266+Ulrike73@users.noreply.github.com>
2019-11-12 15:29:13 +0100

    Update BATGA - soll aber bitte verworfen werden

--- src/main/java/BATGA.java
+++ src/main/java/BATGA.java
@@ -8,16 +8,16 @@ public class BATGA {
         Scanner scanner = new Scanner(System.in);
         Authentication aut = new Authentication();
 
-        while (true) {
-            System.out.println("1 - Log in");
-            System.out.println("0 - quit");
-            int choice = Integer.parseInt(scanner.nextLine());
-            switch (choice) {
-                case 0:
-                    break;
-                case 1:
-                    aut.handleAuthentication();
-            }
-        }
+//        while (true) {
+//            System.out.println("1 - Log in");
+//            System.out.println("0 - quit");
+//            int choice = Integer.parseInt(scanner.nextLine());
+//            switch (choice) {
+//                case 0:
+//                    break;
+//                case 1:
+//                    aut.handleAuthentication();
+//            }
+//        }
     }
 }
