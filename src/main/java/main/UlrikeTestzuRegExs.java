1fa25d4b9f679090e543c3a8f584b3af27a84eab
Ulrike Klaus <55379266+Ulrike73@users.noreply.github.com>
2019-11-18 15:03:29 +0100

    Update UlrikeTestzuRegExs.java

--- src/main/java/main/UlrikeTestzuRegExs.java
+++ src/main/java/main/UlrikeTestzuRegExs.java
@@ -11,11 +11,14 @@ public class UlrikeTestzuRegExs {
         System.out.println("Deine Eingabe enthält nur Zahlen: mit \\d* " + (Pattern.matches("\\d*", eingabe)));
         System.out.println("Deine Eingabe enthält nur Zahlen: mit [0-9]*; das * = keinmal oder beliebig oft: " + (Pattern.matches("[0-9]*", eingabe)));
         do {
-            System.out.println("Mit [yjn] könnte man die abfragen auf j/n vereinfachen - oder?");
+            System.out.print("Mit [yjn] könnte man die abfragen auf y/j/n vereinfachen - oder?");
             eingabe = sc.nextLine();
             System.out.println("Deine Eingabe enthält nur yYnNjJ ? " + (Pattern.matches("[YNJ]*", eingabe.toUpperCase())));
         }while (!Pattern.matches("[YNJ]*", eingabe.toUpperCase()));
         System.out.println("so isses brav :-)");
+        System.out.println("man kann auch bestimmte Zeichen ausschließen - z. B. der gängige Scherz - Sag mal Postbote ohne o");
+        System.out.println("mit Pattern.matches(\"[^o]*, Postbote    kommt heraus: " + (Pattern.matches("[^o]*", "Postbote")));
+        System.out.println("mit Pattern.matches(\"[^o]*, Briefträger kommt heraus: " + (Pattern.matches("[^o]*", "Briefträger")));
 
     }
 }
