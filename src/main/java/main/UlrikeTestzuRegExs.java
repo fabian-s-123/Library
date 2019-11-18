abec5605eceabd0f5fcfee6657fc013a1bca225d
Ulrike Klaus <55379266+Ulrike73@users.noreply.github.com>
2019-11-18 14:51:06 +0100

    Update UlrikeTestzuRegExs.java

--- src/main/java/main/UlrikeTestzuRegExs.java
+++ src/main/java/main/UlrikeTestzuRegExs.java
@@ -8,7 +8,14 @@ public class UlrikeTestzuRegExs {
         Scanner sc = new Scanner(System.in);
         System.out.println("Bitte gib etwas ein: ");
         String eingabe = sc.nextLine();
-        System.out.println("Deine Eingabe enthält nur Zahlen: " + (Pattern.matches("\\d*", eingabe)));
+        System.out.println("Deine Eingabe enthält nur Zahlen: mit \\d* " + (Pattern.matches("\\d*", eingabe)));
+        System.out.println("Deine Eingabe enthält nur Zahlen: mit [0-9]*; das * = keinmal oder beliebig oft: " + (Pattern.matches("[0-9]*", eingabe)));
+        do {
+            System.out.println("Mit [yjn] könnte man die abfragen auf j/n vereinfachen - oder?");
+            eingabe = sc.nextLine();
+            System.out.println("Deine Eingabe enthält nur yYnNjJ ? " + (Pattern.matches("[YNJ]*", eingabe.toUpperCase())));
+        }while (!Pattern.matches("[YNJ]*", eingabe.toUpperCase()));
+        System.out.println("so isses brav :-)");
 
     }
 }
